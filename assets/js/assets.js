4e31f8d78c79651792224d91308c3b1611b73307
KartoffelChips <jan@strassburger.org>
2024-05-14 14:23:37 +0200

    Removed loader

--- assets/js/assets.js
+++ assets/js/assets.js
@@ -11,17 +11,6 @@ const cropNames = {
     "9": "Wheat"
 };
 
-/*--- loader ---*/
-window.addEventListener("load", () => {
-    const loader = document.querySelector(".loader");
-
-    loader.classList.add("loader--hidden");
-
-    loader.addEventListener("transitionend", () => {
-        document.body.removeChild(loader);
-    });
-});
-
 /*--- modals ---*/
 var closeBTN = document.getElementsByClassName("close")[0];
 
@@ -30,17 +19,6 @@ function toggleModal(modal_id) {
     if (modal.classList.contains("_shown")) modal.classList.remove("_shown");
     else modal.classList.add("_shown");
     document.body.scrollTop = document.documentElement.scrollTop = 0;
-}
-
-function CopyToClipboard(id) {
-    navigator.clipboard.writeText("Kartoffelchips#0445")
-        .then(() => {
-        console.log("Text copied to clipboard")
-        })
-        .catch(err => {
-        console.log('Something went wrong', err);
-        })
-    alert('Discord Tag in die Zwichenablage Kopiert');
 }
 
 /*--- main functions ---*/
@@ -224,7 +202,7 @@ function createAlarm(timestamp) {
             body: `${cropNames[contest.dataset.cropone]}, ${cropNames[contest.dataset.croptwo]} and ${cropNames[contest.dataset.cropthree]}`,
             icon: "./crops/9.png"
         });
-        //alarmbtn.innerHTML = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#fefefe"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 5.5C14.7614 5.5 17 7.73858 17 10.5V12.7396C17 13.2294 17.1798 13.7022 17.5052 14.0683L18.7808 15.5035C19.6407 16.4708 18.954 18 17.6597 18H6.34025C5.04598 18 4.35927 16.4708 5.21913 15.5035L6.4948 14.0683C6.82022 13.7022 6.99998 13.2294 6.99998 12.7396L7 10.5C7 7.73858 9.23858 5.5 12 5.5ZM12 5.5V3M10.9999 21H12.9999" stroke="#fefefe" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`;
-        alarmbtn.innerHTML = "NoNotify3"
+        alarmbtn.innerHTML = `<svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" stroke="#fefefe"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M12 5.5C14.7614 5.5 17 7.73858 17 10.5V12.7396C17 13.2294 17.1798 13.7022 17.5052 14.0683L18.7808 15.5035C19.6407 16.4708 18.954 18 17.6597 18H6.34025C5.04598 18 4.35927 16.4708 5.21913 15.5035L6.4948 14.0683C6.82022 13.7022 6.99998 13.2294 6.99998 12.7396L7 10.5C7 7.73858 9.23858 5.5 12 5.5ZM12 5.5V3M10.9999 21H12.9999" stroke="#fefefe" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>`;
+        //alarmbtn.innerHTML = "NoNotify3"
     }, timeBetween)
 }
